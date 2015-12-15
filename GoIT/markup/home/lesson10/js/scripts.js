e8f7644b9a1b0c017a5a59ffa3c85c19803435e9
barbarosa30 <barbarosa30@ukr.net>
2015-12-15 21:25:50 +0200

    fix error

--- GoIT/markup/home/lesson10/js/scripts.js
+++ GoIT/markup/home/lesson10/js/scripts.js
@@ -1,16 +1,26 @@
+var a, b, result;
+var ask = 0;
+
+do {
+	if (ask == 1) {
+		alert('Поле не может быть пустым!');
+	};
+a = prompt('Введите число');
+b = prompt('Введите степень');
+ask = 1;
+} while ( (a === "") || (b === "") || (a === null) || (b === null) );
+result = stepen(a, b);
+console.log('Результат = ', result);
 
 function stepen(a, b) {
-	var i = 0;
-	var result = 1;
-	while (i < b) {
-		result *= a;
-		i++;
-	}
-	return result;
-}
-
-var a, b, result;
-a = prompt ('Введите число');
-b = prompt ('Введите степень');
-result = stepen(a, b);
-alert (result);
+    var i = 0;
+    var result = 1;
+    while (i < Math.abs(b)) {
+        result *= a;
+        i++;
+    };
+    if (b < 0) {
+        result = 1 / result; // минусовая степень
+    };
+    return result;
+};
--- GoIT/markup/home/lesson10_1/js/scripts.js
+++ GoIT/markup/home/lesson10_1/js/scripts.js
@@ -1,17 +1,31 @@
 var arrName = [];
+var proverka;
 for (var i = 0; i < 5; i++) {
-	arrName[i] = prompt('Заполните имена:', 'Вася' );
-}
+	 proverka = prompt('Заполните имена:', 'Вася' );
+	   while ( (proverka == "") || (proverka == null) ) {
+	 	alert('Поле не может быть пустым');
+	 	proverka = prompt('Заполните имена:', 'Вася');
+	   };
+	 arrName[i] = proverka;
+};
 
-var name = prompt('Введите имя:');
+var name, ask;
+ask = 0;
+do {
+	if (ask == 1) {
+      alert('Поле не может быть пустым');
+	};
+  name = prompt('Введите имя:');	
+  ask = 1;
+} while ( (name === "") || (name === "null") ); 
 var counter = 0;
 for (var i = 0; i < arrName.length; i++) {
 	if (arrName[i] == name) {
 		alert(name + ', вы успешно вошли');
 		counter = 1;
 		break;
-        } 
-}
+        }; 
+};
 if (counter == 0) {
 alert('Введенное имя пользователя не существует в массиве ');
-}
+};
