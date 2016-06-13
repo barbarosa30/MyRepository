ee46649caa8634fea0db2d3d332b6a002bd4731a
barbarosa30 <barbarosa30@ukr.net>
2016-06-13 05:19:12 +0300

    new file

---
+++ GoIT/markup/home/lesson11/js/scripts.js
@@ -0,0 +1,8 @@
+function createHTML () {
+  var div = document.createElement('div');
+      h1 = document.createElement('h1');
+      p = document.createElement('p');
+      span = document.createElement('span');
+      form = document.createElement('form');
+      input = document.createElement('input');
+};
